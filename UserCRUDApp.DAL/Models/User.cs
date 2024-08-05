c99a1acf1ab11f8f793e252fec6c9fdf61ebe07b
vigne <vigne@LAPTOP-FTNE1HBJ>
2024-08-05 21:37:13 +0530

    Added User Model

---
+++ UserCRUDApp.DAL/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCRUDApp.DAL.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
--- UserCRUDApp.PresentationLayer/Program.cs
+++ UserCRUDApp.PresentationLayer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using UserCRUDApp.DAL.Models;
+
 namespace UserCRUDApp.PresentationLayer
 {
     public class Program
@@ -7,6 +10,10 @@ namespace UserCRUDApp.PresentationLayer
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
